613b94275d0af0c0b894ddcbf76488ea09c2f317
JoyWambui <joywambuindegwa@gmail.com>
2022-03-12 18:35:48 +0300

    Print the alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,13 +9,11 @@
 int main(void)
 {
 	int small;
-	int new_line;
 
 	for (small = 97; small < 123; small++)
 	{
 		putchar(small);
 	}
-	new_line = 32;
-	putchar(32);
+	putchar(13);
 	return (0);
 }
