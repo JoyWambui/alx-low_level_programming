e775a325f753d3f35baf5ba53a484b5fc5a2025b
JoyWambui <joywambuindegwa@gmail.com>
2022-03-16 19:26:37 +0300

    alphabet task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 
 /**
  * print_alphabet - Prints the alphabet in lowercase followed by a new line.
- * 
+ *
  * Return: Always 0.
  */
 
@@ -12,7 +12,7 @@
 void print_alphabet(void)
 {
 	char alphabet;
-	
+
 	for (alphabet = 97; alphabet < 123; alphabet++)
 	{
 		_putchar(alphabet);
