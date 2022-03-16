e5b9eeaddca9ac00398c13ba20ae72220225f300
JoyWambui <joywambuindegwa@gmail.com>
2022-03-16 18:18:15 +0300

    putchar task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,27 +9,23 @@ int main(void)
 {
 	int n;
 	int i;
-	int j;
 
 	for (n = 48; n < 58; n++)
 	{
-		for (i = 48; i < 58; i++)
+		for (i = 49; i < 58; i++)
 		{
-			for (j = 49; j < 58; j++)
+			putchar(n);
+			putchar(n);				
+			putchar(32);
+			putchar(n);
+			putchar(i);  
+			if (n == 57 && i == 57)
 			{
-				putchar(n);
-				putchar(n);
+			}
+			else
+			{
 				putchar(32);
-				putchar(i);
-				putchar(j);  
-				if (n == 57 && i == 57 && j == 57)
-				{
-				}
-				else
-				{
-					putchar(32);
-					putchar(44);
-				}
+				putchar(44);
 			}
 		}
 	}
