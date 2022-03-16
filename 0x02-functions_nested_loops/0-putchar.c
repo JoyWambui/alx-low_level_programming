fd37474e807b65c36f64b5c563d4b4f0b3db69d3
JoyWambui <joywambuindegwa@gmail.com>
2022-03-16 19:16:46 +0300

    putchar task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,8 +3,8 @@
 
 /**
  * main - Entry point of the program.
- * 
- * Return - (0). Success
+ *
+ * Return: (0). Success
  */
 
 int main(void)
@@ -12,6 +12,7 @@ int main(void)
 	_putchar(95);
 	_putchar(112);
 	_putchar(117);
+	_putchar(116);
 	_putchar(99);
 	_putchar(104);
 	_putchar(97);
