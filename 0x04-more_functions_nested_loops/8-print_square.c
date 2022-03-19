7120d69860cc9ace52d993432797edcedb2c1744
JoyWambui <joywambuindegwa@gmail.com>
2022-03-19 04:49:45 +0300

    You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - draws a square in the terminal
+ * @size: the size of the square.
+ *
+ *Return: Always (0) Success
+ */
+
+
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+		}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
