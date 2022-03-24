1b5f6017b33c8e2d739c0789a2719879430c5440
JoyWambui <joywambuindegwa@gmail.com>
2022-03-24 15:30:19 +0300

    to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,25 +3,20 @@
 /**
  * string_toupper- changes all lowercase letters of a string to uppercase.
  *@char: pointer to characters to be changed
- * 
+ *
  * Return: a char
  */
 
 char *string_toupper(char *str)
 {
-	char upper[100];
-	int i;
-
-	for (i = 0; i != '\0'; i++)
+	while (*str != '\0')
 	{
-		if (str[i] >= 97 && str[i] <= 122)
+		if (*str >= 97 && *str <= 123)
 		{
-			str[i] -= 32;
+			*str -= 32;
 		}
+
+		str++;
 	}
-	for (i = 0; i != '\0'; i++)
-	{
-		upper[i] = str[i];
-	}
-	return (upper);
+	return (str);
 }
