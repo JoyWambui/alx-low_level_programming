8eee7c8facdbae5f79cabbc242ced71a0e53c32c
JoyWambui <joywambuindegwa@gmail.com>
2022-03-30 23:02:08 +0300

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _pow_recursion - raises x to y.
- * @x: base number.
- * @y: power.
- * Return: value of x raised to y.
+ * _sqrt_recursion - finds the square root of a number
+ * @n: number whose squareroot is being found.
+ * Return: square root.
  */
 
 int _sqrt_recursion(int n)
@@ -12,8 +11,13 @@ int _sqrt_recursion(int n)
 	int i = 1;
 	int sqrt_wrapper(int n, int i);
 
-	return sqrt_wrapper(n, i);
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (sqrt_wrapper(n, i));
 }
+
 int sqrt_wrapper(int n, int i)
 {
 	if (i * i == n)
@@ -23,6 +27,6 @@ int sqrt_wrapper(int n, int i)
 	else
 	{
 		i += 1;
-		return sqrt_wrapper(n, i);
+		return (sqrt_wrapper(n, i));
 	}
 }
