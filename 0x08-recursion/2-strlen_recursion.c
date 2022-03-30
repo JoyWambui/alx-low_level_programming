54e6345296399882fbf5761dd11d05d580e56c5c
JoyWambui <joywambuindegwa@gmail.com>
2022-03-30 20:55:46 +0300

    length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,9 +12,8 @@ int _strlen_recursion(char *s)
 	int n = 0;
 	if (*s != '\0')
 	{
-		n += 1;
-		_strlen_recursion(s + 1);
-		
+		n++;
+		return n;
 	}
-	return (n);
+	return (_strlen_recursion(s+1));
 }
