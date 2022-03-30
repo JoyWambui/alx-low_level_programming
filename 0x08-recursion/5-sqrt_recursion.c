30b925100c51c148dc9db6ff0b69004c0bf73045
JoyWambui <joywambuindegwa@gmail.com>
2022-03-30 23:09:41 +0300

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,13 +3,9 @@
 /**
  * _sqrt_recursion - finds the square root of a number
  * @n: number whose squareroot is being found.
- * Return: wrapper function. 
- * 
+ * Return: wrapper function.
+ *
  * sqrt_wrapper - compares a number with the square of another.
- * @n: number to be compared to.
- * @i: number to be squared.
- * Return: square root of first number.
-
  */
 
 int _sqrt_recursion(int n)
