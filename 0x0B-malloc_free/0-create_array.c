8673124877e2175daa241bec962e491893caeac2
JoyWambui <joywambuindegwa@gmail.com>
2022-04-05 22:47:38 +0300

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	arr = (char *)malloc(sizeof(*arr) * size);
+	arr = malloc(sizeof(*arr) * size);
 	if (arr == NULL)
 	{
 		return (NULL);
