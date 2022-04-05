61caabbe4e1b7a7691a750d736a455648c5e1a41
JoyWambui <joywambuindegwa@gmail.com>
2022-04-05 23:54:39 +0300

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,6 +19,14 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}	
 	while (*s1 != '\0')
 	{
 		dest[i] = *s1;
