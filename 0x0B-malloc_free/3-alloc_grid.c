064ed3e6d4163fbf169da99046af37f8e374c80b
JoyWambui <joywambuindegwa@gmail.com>
2022-04-06 12:53:07 +0300

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,50 +1,40 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
-#include <string.h>
-
 /**
- * alloc_grid- returns a pointer to a 2 dimensional array of integers.
- * @width: array in 2d array.
- * @height: array in 2d array.
- * Return: pointer to a 2 dimensional array of integers.
+ * alloc_grid - nested loop to make grid
+ * @width: width input
+ * @height: height input
+ * Return: pointer to 2 dim. array
  */
-
 int **alloc_grid(int width, int height)
 {
-	int **pointer_array;
-	int i;
-	int j;
-	int k;
+	int **iarray;
+	int i, n;
 
-	if ((width == 0 || width < 0) || (height == 0 || height < 0))
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	iarray = malloc(sizeof(int) * height);
+
+	if (iarray == NULL)
 	{
+		free(iarray);
 		return (NULL);
 	}
-	pointer_array = (int **) malloc(height * sizeof(int *));
-	if (pointer_array == NULL)
+
+	for (i = 0; i < height; i++)
 	{
-		return (NULL);
-	}
-	pointer_array[0] = malloc(height * width * sizeof(int));
-	for (i = 1; i < height; i++)
-	{
-		pointer_array[i] = pointer_array[0] + i * width;
-	}
-	if (pointer_array)
-	{
-		for (j = 0; j < height; j++)
+		iarray[i] = malloc(sizeof(int) * width);
+		if (iarray == NULL)
 		{
-			for (k = 0; k < width; k++)
-			{
-				pointer_array[i][j] = 0;
-			}
+			free(iarray);
+			return (NULL);
+		}
+
+		for (n = 0; n < width; n++)
+		{
+			iarray[i][n] = 0;
 		}
 	}
-	else
-	{
-		for (i = 0; i < height; i++)
-			free(pointer_array[i]);
-		free(pointer_array);
-	}
-	return (pointer_array);
+	return (iarray);
 }
